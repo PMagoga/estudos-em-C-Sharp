ae63a08481bbaec0a6d20daa5d37e52f833e3317
PMagoga <pmagoga@gmail.com>
2025-01-14 16:41:02 -0300

    tratamento de exceções

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Listas;
+
+List<Aluno> alunos = FonteDados.GetAlunos();
+
+// criação do método estático para exibir os alunos
+ExibirAlunos(alunos);
+
+// criação dos objetos alunos Bia e Mario
+Aluno bia = new Aluno() { Nome = "Bia", Nota = 7.75};
+Aluno mario = new Aluno() { Nome = "Mario", Nota = 8.95};
+alunos.Add(bia);
+alunos.Add(mario);
+
+ExibirAlunos(alunos);
+
+// remover um aluno da lista, de acordo com o nome
+var aluno1 = alunos.Find(n => n.Nome.Equals("Amanda"));
+alunos.Remove(aluno1);
+
+ExibirAlunos(alunos);
+
+// para ordenar os alunos não podemos usar o método sort, pois alunos é um objeto
+var listaOrdenada = alunos.OrderBy(n => n.Nome).ToList();
+ExibirAlunos(listaOrdenada);
+
+// exibir alunos com nota maior que 8
+var alunosNotaOito = alunos.FindAll(n => n.Nota >= 8);
+Console.Write("\nAlunos com nota maior que 8\n");
+foreach (var aluno in alunosNotaOito)
+{
+    Console.WriteLine($"{aluno.Nome}\t{aluno.Nota}");
+}
+
+Console.ReadKey();
+
+static void ExibirAlunos(List<Aluno> alunos)
+{
+    Console.WriteLine("\nRelação de Alunos\n");
+    Console.WriteLine("\nNome\tNota");
+
+    double somaNotas = 0.0;
+
+    foreach (var aluno in alunos)
+    {
+        Console.WriteLine($"{aluno.Nome}\t{aluno.Nota}");
+        somaNotas += aluno.Nota;
+    }
+
+    double mediaNota = somaNotas / alunos.Count;
+    Console.WriteLine($"\nMédia das Notas: {Math.Round(mediaNota, 2)}");
+    Console.WriteLine($"\nTotal de Alunos: {alunos.Count()}");
+}
---
+++ Listas/TratamentoExcecoes/Program.cs
@@ -0,0 +1,29 @@
+﻿
+try
+{
+	Console.WriteLine("\nInforme o dividendo");
+	int dividendo = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("\nInforme o divisor");
+	int divisor = Convert.ToInt32(Console.ReadLine());
+	int resultado = (dividendo / divisor);
+	Console.WriteLine($"\n{dividendo}/{divisor} = {resultado}\n");
+	Console.ReadKey();
+
+}
+catch (DivideByZeroException) when (DateTime.Now.DayOfWeek == DayOfWeek.Tuesday)
+{
+
+	Console.WriteLine("\nHoje é sexta-feira e não capturamos estas exceções por motivos óbvios.. \n\nSextouuu\n");
+}
+catch(Exception ex) when (ex.Message.Contains("format"))
+{
+	Console.WriteLine("filtro de exceção com format");
+	Console.WriteLine(ex.Message);
+}
+catch(Exception ex)
+{
+	Console.WriteLine(ex.Message);
+	Console.WriteLine(ex.StackTrace);
+}
+
+Console.ReadKey();
